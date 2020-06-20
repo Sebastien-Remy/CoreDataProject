46ff3c8cf7f448ac7e818890469e4c227cbb0383
Sebastien REMY <seb.remy@me.com>
2020-06-20 18:15:48 +0200

    Dynamically filtering @FetchRequest with SwiftUI

---
+++ Singer+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Singer+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by Sebastien REMY on 20/06/2020.
+//  Copyright © 2020 MonkeyDev. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Singer)
+public class Singer: NSManagedObject {
+
+}
